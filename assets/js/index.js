36b6112332e4f478a40fd4620a53aa28e8fc6cc8
Ana <anacarolineofc130@gmail.com>
2023-11-10 18:06:29 -0300

    Adicionado o FAQ

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+function toggleAnswer(id) {
+    var answer = document.getElementById(id);
+    if (answer.style.display === 'block') {
+        answer.style.display = 'none';
+    } else {
+        answer.style.display = 'block';
+    }
+}
