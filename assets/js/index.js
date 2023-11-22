7f871e6e3546bc5d7ae73dbd0193b329d394c53a
Fernanda Novello <110908782+fernandanovello@users.noreply.github.com>
2023-11-22 02:25:44 -0300

    Delete assets directory

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-function toggleAnswer(id) {
-    var answer = document.getElementById(id);
-    if (answer.style.display === 'block') {
-        answer.style.display = 'none';
-    } else {
-        answer.style.display = 'block';
-    }
-}
